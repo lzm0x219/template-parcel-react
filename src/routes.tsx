50168d430c7150c16e66cb35d5b6133ad1f184e9
0x219 <lzm0x219@gmail.com>
2022-11-12 21:48:28 +0800

    Update routes file

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,18 +1,24 @@
 import { lazy, Suspense } from "react";
-import { createRoutesFromElements, Route } from "react-router-dom";
+import { type RouteObject } from "react-router-dom";
 import App from "@/App";
 
 const Index = lazy(() => import("@/routes/index"));
 
-export default createRoutesFromElements([
-  <Route path="/" element={<App />}>
-    <Route
-      index
-      element={
-        <Suspense>
-          <Index />
-        </Suspense>
-      }
-    />
-  </Route>,
-]);
+const routes: RouteObject[] = [
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: (
+          <Suspense>
+            <Index />
+          </Suspense>
+        ),
+      },
+    ],
+  },
+];
+
+export default routes;
